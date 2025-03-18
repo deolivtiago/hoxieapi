253dd983f8120267d445a6a0a871626cff818e90
Tiago de Oliveira <deolivtiago@outlook.com>
2025-03-18 12:47:34 -0300

    chore: init spring web app

---
+++ src/test/java/com/olvrslab/hoxieapi/HoxieapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olvrslab.hoxieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoxieapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
